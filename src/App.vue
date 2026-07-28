f2feb0ef49fabc5cb25342926d0431b024cb848c
ClarkGodwin <clarkniyonzima@gmail.com>
2026-07-28 07:49:52 +0200

    header done

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Settings, SettingsIcon } from '@lucide/vue';
+</script>
 
 <template>
-  <h1 class="">
-    Welcome in our Promodoro Timer
-  </h1>
+  <div class="flex justify-between items-center w-header mx-auto py-2.5">
+    <h1 class=" text-frosted text-header">
+      Welcome in our Promodoro Timer
+    </h1>
+    <SettingsIcon class=" text-frosted size-(--text-header)"/>
+  </div>
 </template>
 
-<style scoped></style>
