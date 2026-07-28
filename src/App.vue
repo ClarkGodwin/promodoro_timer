24e1e7d314ddb6caff0849b155229d7abc59bf44
ClarkGodwin <clarkniyonzima@gmail.com>
2026-07-28 10:42:57 +0200

    home router made

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import { SettingsIcon } from '@lucide/vue';
-import Timer from './components/Timer.vue';
 </script>
 
 <template>
   <header class="flex justify-between items-center w-header mx-auto py-2.5">
-    <h1 class=" text-frosted text-header">
+    <RouterLink :to="{ name: 'home' }" class=" text-frosted text-header">
       Welcome in our Promodoro Timer
-    </h1>
+    </RouterLink>
     <SettingsIcon class=" text-frosted size-(--text-header)"/>
   </header>
 
-  <Timer/>
+  <RouterView />
 </template>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,17 @@
+import Timer from '@/components/Timer.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  { 
+    path: '/', 
+    name: 'home',
+    component: () => Timer
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes
 })
 
 export default router
