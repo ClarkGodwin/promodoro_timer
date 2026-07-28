bada6117cbf4a3fc64d081eb61f9e7168c7b92a2
ClarkGodwin <clarkniyonzima@gmail.com>
2026-07-28 09:34:26 +0200

    Timer for mobile done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import { Settings, SettingsIcon } from '@lucide/vue';
+import { SettingsIcon } from '@lucide/vue';
+import Timer from './components/Timer.vue';
 </script>
 
 <template>
-  <div class="flex justify-between items-center w-header mx-auto py-2.5">
+  <header class="flex justify-between items-center w-header mx-auto py-2.5">
     <h1 class=" text-frosted text-header">
       Welcome in our Promodoro Timer
     </h1>
     <SettingsIcon class=" text-frosted size-(--text-header)"/>
-  </div>
+  </header>
+
+  <Timer/>
 </template>
 
---
+++ src/components/Timer.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { PauseCircle, PlayCircle, Square } from '@lucide/vue';
+</script>
+
+<template>
+    <div class="flex flex-col justify-between items-center w-timer h-timer mx-auto my-4 py-4  bg-surface-300 rounded-2xl">
+        <div 
+        class="flex gap-3.5 items-center *:text-text-muted *:bg-surface-100 *:px-2 *:py-1 *:rounded-xl *:border *:border-transparent *:hover:border-frosted *:hover:text-frosted *:text-[11px] *:text-center"
+        >
+            <button>Work Session</button>
+            <button>Short Break</button>
+            <button>Long Break</button>
+        </div>
+
+        <span class="text-frosted text-[105px]">25:09</span>
+
+        <div class="flex gap-2 items-center *:flex *:items-center *:gap-0.5 *:bg-surface-100 *:text-text-muted *:px-1 *:py-0.5 *:rounded-xl *:text-timer-man">
+            <button>
+                <PauseCircle class=" size-(--text-timer-man)"/>
+                <span>Pause</span>
+            </button>
+            <button>
+                <PlayCircle class=" size-(--text-timer-man)"/>
+                <span>Resume</span>
+            </button>
+            <button>
+                <Square class=" size-(--text-timer-man)"/>
+                <span>Stop</span>
+            </button>
+        </div>
+    </div>
+</template>
