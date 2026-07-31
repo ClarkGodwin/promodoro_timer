aa5ed5d8793428084537ce81e3d58b395d36b999
ClarkGodwin <clarkniyonzima@gmail.com>
2026-07-31 16:17:16 +0200

    header of the setting s page created

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
+    <section class="bg-surface-300">
+        <h2>Setting's page</h2>
+        <p>
+            Here you can modify the value the time of each session and even the number of work session before the long break session
+        </p>
+
+    </section>
 </template>
