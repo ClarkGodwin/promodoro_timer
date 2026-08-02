6d0701db20d9451309418d0a67a4f7d69ec8949d
ClarkGodwin <clarkniyonzima@gmail.com>
2026-08-02 21:14:41 +0200

    toast congigured

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -3,8 +3,8 @@ import { CircleCheck } from '@lucide/vue';
 </script>
 
 <template>
-    <div class="absolute top-0 left-left-toaster flex gap-1 text-white font-bold bg-frosted w-[200px] mx-auto">
-        <CircleCheck/>
+    <div class="absolute top-0 left-toast flex gap-1 items-center text-white font-bold bg-frosted w-toast mx-auto p-p-toast rounded-2xl text-toast">
+        <CircleCheck class="size-(--text-toast)"/>
         <span>Changes were saved</span>
     </div>
 </template>
